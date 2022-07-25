888c6e04e41b37e23fdbdd75d843599753dce873
jaykumar-bhatt <jaykumar.bhatt@bacancy.com>
2022-07-25 16:24:06 +0530

    add: validation while transaction

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import * as transactionController from '../controllers/transaction/transaction.controller';
 import authentication from '../middlewares/authentication';
+import { addTransactionValidation, addGroupTransactionValidation } from '../controllers/transaction/transaction.validator';
 
 const router = express.Router();
 
 router.get('/', authentication, transactionController.addTransactionView);
-router.post('/', authentication, transactionController.addTransaction);
+router.post('/', authentication, addTransactionValidation, transactionController.addTransaction);
 router.get('/group', authentication, transactionController.showAddGroupTransaction);
-router.post('/group', authentication, transactionController.addGroupTransaction);
+router.post('/group', authentication, addGroupTransactionValidation, transactionController.addGroupTransaction);
 
 router.get('/owes', authentication, transactionController.getOwesTransaction);
 router.get('/borrow', authentication, transactionController.getBorrowsTransaction);
