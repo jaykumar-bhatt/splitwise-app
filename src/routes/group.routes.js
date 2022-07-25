58b7c4fdfc9d6a5e7bd6d855bb1d05baa9fcc58f
jaykumar-bhatt <jaykumar.bhatt@bacancy.com>
2022-07-25 16:06:36 +0530

    add: Group module validation

--- src/routes/group.routes.js
+++ src/routes/group.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import * as groupController from '../controllers/group/group.controller';
 import authentication from '../middlewares/authentication';
+import { addGroupValidation } from '../controllers/group/group.validator';
 
 const router = express.Router();
 
 router.get('/add', authentication, groupController.addGroupShow);
-router.post('/', authentication, groupController.addGroup);
+router.post('/', authentication, addGroupValidation, groupController.addGroup);
 router.get('/', authentication, groupController.getGroup);
 router.get('/delete', authentication, groupController.deleteGroup);
 
